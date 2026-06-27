b78934dae1e3ba3d1a7c0fd825b308778ed5b2ce
MohammedHabibQureshi <mdhabib.qureshi786@gmail.com>
2026-06-27 15:22:27 +0530

    Create day1.py

---
+++ Day-01-Variables-DataTypes-Operators/day1.py
@@ -0,0 +1,62 @@
+# Variables
+name = "Mohammed Habib"
+age = 22
+salary = 45000.50
+is_student = True
+
+print("Variables")
+print(name)
+print(age)
+print(salary)
+print(is_student)
+
+# Type Checking
+print("\nType Checking")
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_student))
+
+# Type Conversion
+print("\nType Conversion")
+num = "100"
+print(int(num))
+print(float(num))
+print(str(100))
+
+# Arithmetic Operators
+a = 15
+b = 4
+
+print("\nArithmetic Operators")
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+# Comparison Operators
+print("\nComparison Operators")
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical Operators
+print("\nLogical Operators")
+print(a > 10 and b < 10)
+print(a < 10 or b < 10)
+print(not(a > 10))
+
+# String Concatenation
+first = "Mohammed"
+last = "Habib"
+
+print("\nString Concatenation")
+print(first + " " + last)
+
+# f-Strings
+print("\nf-Strings")
+print(f"My name is {first} {last} and I am {age} years old.")
